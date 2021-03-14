08f121ed3a80f25bc9e8c9800a514eb7a055b13b
Naama Carmel <naama271@gmail.com>
2021-03-14 13:11:52 +0200

    first

---
+++ my-app/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// 
+
+const App = function (){
+return <div>
+  <h2>AppleSeeds
+React App</h2>
+<a href="#">click me</a>
+  
+   </div>
+}
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+ReactDOM.render(
+<App />, document.querySelector("#root")
+);
