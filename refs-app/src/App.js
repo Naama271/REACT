cba112226740b56e4d355ea84d0268a36861d225
Naama Carmel <naama271@gmail.com>
2021-03-23 21:49:02 +0200

    14.1 refs

---
+++ refs-app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          ref={this.textInput} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
